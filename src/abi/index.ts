29dd5130839be2f1ff349da2b17d2a8277774d41
Johnson Chen <chnejohnson@gmail.com>
2021-09-29 16:42:08 +0800

    add: abi and typechain

---
+++ src/abi/index.ts
@@ -0,0 +1,23 @@
+import { abi as ERC20 } from './ERC20.json'
+import { abi as FundingRoundFactory } from './FundingRoundFactory.json'
+import { abi as FundingRound } from './FundingRound.json'
+import { abi as MACI } from './MACI.json'
+import { abi as UserRegistry } from './IUserRegistry.json'
+import { abi as BrightIdUserRegistry } from './BrightIdUserRegistry.json'
+import { abi as SimpleRecipientRegistry } from './SimpleRecipientRegistry.json'
+import { abi as OptimisticRecipientRegistry } from './OptimisticRecipientRegistry.json'
+import { abi as KlerosGTCR } from './IKlerosGTCR.json'
+import { abi as KlerosGTCRAdapter } from './KlerosGTCRAdapter.json'
+
+export {
+  ERC20,
+  FundingRoundFactory,
+  FundingRound,
+  MACI,
+  UserRegistry,
+  BrightIdUserRegistry,
+  SimpleRecipientRegistry,
+  OptimisticRecipientRegistry,
+  KlerosGTCR,
+  KlerosGTCRAdapter,
+}
